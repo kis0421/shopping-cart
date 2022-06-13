562d21ddbd2f385992b9e9eb16c572ae8072c492
kis0421 <kig0421@gmail.com>
2022-06-13 23:24:46 +0900

    update 렌더링되도록 element 예외처리추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client"
 
 import App from "./App";
 
-const rootNode: Element = document.querySelector("#root")!;
+const rootNode = document.querySelector("#root");
+if (!rootNode) {
+  throw new Error("root element not found");
+}
 createRoot(rootNode).render(
   <StrictMode>
     <App />
