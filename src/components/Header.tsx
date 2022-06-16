0291b33986a5dc0200043c97b3f01340ee0be111
kis0421 <kig0421@gmail.com>
2022-06-17 05:16:47 +0900

    update NavLink 추가로 페이지 전환

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
 import { memo } from "react";
 import { css } from "@emotion/react";
 import { ShoppingCart as ShoppingCartIcon } from "@mui/icons-material";
+import { NavLink } from "react-router-dom"
+
+const cartStyle = css`
+  float: right;
+  margin: 4px;
+  color: #777;
+  transition: color .5s;
+  &:hover {
+    color: #333
+  }
+`
 
 export default memo(() => {
   return (
     <header css={css`border-bottom: 1px solid #dedede;`}>
       <div css={css`max-width:1024px;padding:16px;margin:0 auto;`}>
         <div css={css`font-size:1.5rem;`}>
-          <span>Shopping Cart</span>
-          <ShoppingCartIcon css={css`float: right; margin: 4px`} />
+          <NavLink to="/">Shopping Cart</NavLink>
+          <NavLink to="/cart">
+            <ShoppingCartIcon css={cartStyle} />
+          </NavLink >
         </div>
       </div>
     </header>
