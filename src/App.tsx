d646ea65a27f4c1c3d74ffc6374069d0a0117b2d
kis0421 <kig0421@gmail.com>
2022-06-17 05:14:43 +0900

    update App 전역 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query"
 
 import { css } from "@emotion/react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import Header from "./components/Header";
+import Routes from "./Routes";
 
 const queryClient = new QueryClient();
 const appStyle = css`
   font-size:16px;
+  a{
+    color: inherit;
+    &:hover, &:link {
+      text-decoration: none;
+    }
+  }
+
 `;
 const theme = createTheme({
   palette: {
