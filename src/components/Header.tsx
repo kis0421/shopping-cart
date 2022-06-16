28d6a8f30e09c23966dcd4a1e249df939d04b23b
kis0421 <kig0421@gmail.com>
2022-06-17 05:00:46 +0900

    refactor css 분리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,15 @@
-import { css } from "@emotion/react";
 import { memo } from "react";
-
-const headerStyle = css`
-  border-bottom: 1px solid #dedede;
-  > div:nth-of-type(1) {
-    max-width:1024px;
-    padding:16px;
-    margin:0 auto;
-  }
-`;
+import { css } from "@emotion/react";
+import { ShoppingCart as ShoppingCartIcon } from "@mui/icons-material";
 
 export default memo(() => {
-  console.log("aaa")
   return (
-    <header css={headerStyle}>
-      <div>
-        <div css={css`font-size:1.5rem;`}>Shopping Cart</div>
+    <header css={css`border-bottom: 1px solid #dedede;`}>
+      <div css={css`max-width:1024px;padding:16px;margin:0 auto;`}>
+        <div css={css`font-size:1.5rem;`}>
+          <span>Shopping Cart</span>
+          <ShoppingCartIcon css={css`float: right; margin: 4px`} />
+        </div>
       </div>
     </header>
   )
