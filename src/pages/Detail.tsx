d72d2e6196bd35f72193e201de1fac9aa07e23b6
kis0421 <kig0421@gmail.com>
2022-07-12 20:51:33 +0900

    useParams detail id 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,10 @@
 import { memo } from "react";
+import { useParams } from 'react-router-dom';
 
 export default memo(() => {
+  const { id } = useParams();
+
   return (
-    <div>detail</div>
+    <div>detail {id}</div>
   )
 });
